0555009fd2e72deedede0d569122c826b9acae18
Sugam Shrestha <info@shresthasugam.com.np>
2026-05-22 14:57:44 +0545

    feat: pwa integration

--- lib/services/api.ts
+++ lib/services/api.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 import { useAuthStore } from '@/lib/store/auth-store';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || '';
+// const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api';
 
 const api = axios.create({
   baseURL: API_URL,
