7121012ec9da843d2c19a2047b2a66dcdd936ce1
Sugam Shrestha <info@shresthasugam.com.np>
2026-05-30 11:28:35 +0545

    fix: ready to use deployed url

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  metadataBase: new URL(process.env.NODE_ENV === 'development' ? 'https://localhost:3000' : 'https://leapfrog.com'),
+  metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL ?? 'http://localhost:3000'),
   title: 'Leapfrog Connect - LMS',
   description: 'Skills to Jobs Pipeline',
   appleWebApp: {
