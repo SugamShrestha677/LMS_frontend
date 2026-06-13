780466dca4c41a734be15e25176e32fbc56ca512
Sugam Shrestha <info@shresthasugam.com.np>
2026-06-13 17:11:17 +0545

    fix: Updated sitemap url

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://buildandhire.me',
+    sitemap: 'https://buildandhire.me/sitemap.xml',
   };
 }
