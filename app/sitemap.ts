d10b20136dc9368cda05617a2222f2023ac51755
Sugam Shrestha <info@shresthasugam.com.np>
2026-06-13 17:08:17 +0545

    updated domain for sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://skillbridge-eight-iota.vercel.app';
+  const baseUrl = 'https://buildandhire.me';
 
   return [
     {
